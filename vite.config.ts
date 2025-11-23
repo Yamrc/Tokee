a75b811dda7d017dded359acd9111c6639e0fe35
Yamrc <yamrci@outlook.com>
2025-11-23 12:32:53 +0800

    Fix(ui): icon无法在构建产物中正常显示

--- vite.config.ts
+++ vite.config.ts
@@ -11,38 +11,6 @@ const config: UserConfig = {
 	},
 	build: {
 		target: 'esnext',
-		// apexcharts 实在是太大
-		rollupOptions: {
-			treeshake: {
-				moduleSideEffects: (id: string) => {
-					if (id.includes('.css') || id.includes('.scss') || id.includes('.less')) {
-						return true;
-					}
-					if (id.includes('solid-devtools')) {
-						return true;
-					}
-					return false;
-				},
-				preset: 'smallest',
-				propertyReadSideEffects: false,
-				tryCatchDeoptimization: false,
-			},
-			output: {
-				manualChunks(id) {
-					if (id.includes('node_modules')) {
-						if (id.includes('apexcharts')) return 'apexcharts';
-						if (id.includes('iconify-icon')) return 'iconify';
-						if (id.includes('overlayscrollbars')) return 'scrollbars';
-						if (id.includes('solid-js')) return 'solid';
-					}
-					return null;
-				},
-			},
-		},
-	},
-	optimizeDeps: {
-		include: ['solid-js', '@iconify-icon/solid'],
-		exclude: ['apexcharts'],
 	},
 	resolve: {
 		alias: {
