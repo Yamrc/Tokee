087cfc9392f2057e6953766029be4e56a943a598
Yamrc <yamrci@outlook.com>
2025-12-05 22:55:40 +0800

    Fix(UI): 修复Firefox下footer的换行问题

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -7,10 +7,10 @@ const Footer: Component = () => {
 	return (
 		<footer class="max-w-(--page-width) w-full md:px-4 mx-auto mb-4">
 			<div class="transition flex flex-col items-center justify-center px-6">
-				<div class="transition text-50 text-sm text-center">
+				<div class="transition text-50 text-sm text-center whitespace-nowrap">
 					&copy; <span id="copyright-year">{new Date().getFullYear()}</span> {siteConfig.author}. All Rights Reserved.
 				</div>
-				<div class="transition text-50 text-sm text-center">
+				<div class="transition text-50 text-sm text-center whitespace-nowrap">
 					Powered by{' '}
 					<a class="transition link text-(--primary) font-medium" target="_blank" rel="noopener noreferrer" href="https://uptimerobot.com">UptimeRobot</a> &{' '}
 					<a class="transition link text-(--primary) font-medium" target="_blank" rel="noopener noreferrer" href="https://github.com/Yamrc/Tokee">Tokee</a>
