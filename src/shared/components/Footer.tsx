f2caf2a022005891481c22af3848c214b9c85eec
Yamrc <yamrci@outlook.com>
2025-12-24 09:08:12 +0800

    Feat(ui): 调整页脚样式与内容

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,19 +1,23 @@
 import { Component } from 'solid-js';
+import { Icon } from '@iconify-icon/solid';
 import { siteConfig } from '@/config';
 
 const Footer: Component = () => {
-	const current_year = new Date().getFullYear();
-
 	return (
 		<footer class="max-w-(--page-width) w-full md:px-4 mx-auto mb-4">
 			<div class="transition flex flex-col items-center justify-center px-6">
 				<div class="transition text-50 text-sm text-center whitespace-nowrap">
-					&copy; <span id="copyright-year">{new Date().getFullYear()}</span> {siteConfig.author}. All Rights Reserved.
+					<span style={{ 'font-family': "'Roboto', system-ui, sans-serif" }}>©</span> <span id="copyright-year">{new Date().getFullYear()}</span> {siteConfig.author}. All Rights Reserved.
 				</div>
-				<div class="transition text-50 text-sm text-center whitespace-nowrap">
-					Powered by{' '}
-					<a class="transition link text-(--primary) font-medium" target="_blank" rel="noopener noreferrer" href="https://uptimerobot.com">UptimeRobot</a> &{' '}
-					<a class="transition link text-(--primary) font-medium" target="_blank" rel="noopener noreferrer" href="https://github.com/Yamrc/Tokee">Tokee</a>
+				<div class="flex flex-wrap items-center justify-center gap-1 transition text-50 text-sm text-center whitespace-nowrap">
+					<span class="gap-1">
+						Powered by{' '}
+						<a class="transition link text-(--primary) font-medium" target="_blank" rel="noopener noreferrer" href="https://uptimerobot.com">UptimeRobot</a> &{' '}
+						<a class="transition link text-(--primary) font-medium" target="_blank" rel="noopener noreferrer" href="https://github.com/Yamrc/Tokee">Tokee</a>
+					</span>
+					<span class="inline-flex items-center align-baseline gap-1">
+						<Icon icon="material-symbols:commit-rounded" width="0.875rem" height="0.875rem" />{window._git_hash}
+					</span>
 					{siteConfig.record?.enable && (
 						<>
 							{' / '}
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import analyze from 'rollup-plugin-analyzer';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { execSync } from 'child_process';
 import { siteConfig } from './src/config';
 
 const inject_config_plugin = (): Plugin => {
@@ -13,7 +14,8 @@ const inject_config_plugin = (): Plugin => {
 				.replace(/<title>.*?<\/title>/, `<title>${siteConfig.title}</title>`)
 				.replace(/<meta name="description" content=".*?" \/>/, `<meta name="description" content="${siteConfig.description}" />`)
 				.replace(/<meta name="author" content=".*?" \/>/, `<meta name="author" content="${siteConfig.author}" />`)
-				.replace(/<html lang=".*?"/, `<html lang="${siteConfig.lang || 'zh-CN'}" style="--hue: ${siteConfig.hue};--page-width: ${siteConfig.pageWidth}rem"`);
+				.replace(/<html lang=".*?"/, `<html lang="${siteConfig.lang || 'zh-CN'}" style="--hue: ${siteConfig.hue};--page-width: ${siteConfig.pageWidth}rem"`)
+				.replace(/const GIT_HASH = '.*?';/, `const GIT_HASH = '${execSync('git rev-parse --short HEAD', { encoding: 'utf-8' }).trim() || 'abcdefg'}';`);
 		},
 	};
 };
